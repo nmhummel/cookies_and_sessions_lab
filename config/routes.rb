23d3ee45ac9c1b5434955d0cf24ff7b18876258b
nmhummel <natalie.hummel@gmail.com>
2021-01-13 15:00:58 -0600

    commit tree 063e5270a348edae7c84fef544c89a369a9e89cc

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'products#index' # one to display the Products#index (the root route)
+  post '/' => 'products#add' # and one to post the products to add them to the cart.
 end
